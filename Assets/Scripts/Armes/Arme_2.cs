289a1d34f30891a398e5e13d239f74bf9dc113fc
Wylp <Quentin.02300@hotmail.fr>
2022-04-02 19:20:34 +0200

    Ajout

---
+++ Assets/Scripts/Armes/Arme_2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arme_2 : MonoBehaviour
+{
+    public GameObject Bullet;
+    public float FrequenceTir;
+    private float Timer;
+
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if(Timer > FrequenceTir)
+        {
+            Timer = 0;
+            Shot();
+        }
+    }
+
+    void Shot()
+    {
+        GameObject LastBullet = Instantiate(Bullet, transform.position, PlayerManager._playerManager.Player.transform.rotation);
+        LastBullet.transform.parent = PlayerManager._playerManager.Player.transform;
+    }
+}
